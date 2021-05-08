a894b4f823e791151611121c1c8cd877e643b988
Connor <44247002+Jeichi@users.noreply.github.com>
2021-05-08 16:29:53 +0100

    Teacher and Student roles successfully created for version 2. Removal of courses and this feature will be reintroduced for version 3.

--- config/jetstream.php
+++ config/jetstream.php
@@ -46,20 +46,7 @@ return [
         // Features::profilePhotos(),
         // Features::api(),
         // Features::teams(['invitations' => true]),
-        // Features::accountDeletion(),
+        Features::accountDeletion(),
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Profile Photo Disk
-    |--------------------------------------------------------------------------
-    |
-    | This configuration value determines the default disk that will be used
-    | when storing profile photos for your application's users. Typically
-    | this will be the "public" disk but you may adjust this if needed.
-    |
-    */
-
-    'profile_photo_disk' => 'public',
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,13 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 
 Route::group(['middleware' => 'auth'], function() {
-    Route::resource('courses', \App\Http\Controllers\CourseController::class);
+    Route::group(['middleware' => 'role:student', 'prefix' => 'student', 'as' => 'student.'], function() {
+        Route::resource('lessons', \App\Http\Controllers\Students\LessonController::class);
+    });
+   Route::group(['middleware' => 'role:teacher', 'prefix' => 'teacher', 'as' => 'teacher.'], function() {
+       Route::resource('courses', \App\Http\Controllers\Teachers\CourseController::class);
+   });
+    Route::group(['middleware' => 'role:admin', 'prefix' => 'admin', 'as' => 'admin.'], function() {
+        Route::resource('users', \App\Http\Controllers\Admin\UserController::class);
+    });
 });
